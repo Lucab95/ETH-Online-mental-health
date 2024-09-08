de54d52743eb9282c92deaa84ca3bce2e5c9e48d
Enrique <ennriqe@Enriques-MacBook-Pro.local>
2024-09-08 17:15:45 +0200

    webapp last touches det

--- quickstart_complete/client_code/streamlit_app.py
+++ quickstart_complete/client_code/streamlit_app.py
@@ -5,6 +5,21 @@ from torch.utils.data import Dataset
 import os
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 import pandas as pd
+import random
+import numpy as np
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+# Call the set_seed function at the start of your script
+set_seed(4)
+
 class MentalHealthDataset(Dataset):
     def __init__(self, dataframe: pd.DataFrame, transform=None):
         self.data = dataframe.drop(['target', 'total_count'], axis=1)
